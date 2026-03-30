78439292bd27ae56aba42ad4024c3a2f7c836a56
Marcos Leandro Cachinski <47596246+MARCOSLEAN@users.noreply.github.com>
2026-03-30 19:18:20 -0300

    Arquivos atualizados 01

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-import time
+from flask import Flask, jsonify
 
+app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return jsonify({"message": "App rodando com Docker"})
+
+@app.route("/soma/<int:a>/<int:b>")
 def soma(a, b):
-    return a + b
+    return jsonify({"resultado": a + b})
 
-
+@app.route("/subtracao/<int:a>/<int:b>")
 def subtracao(a, b):
-    return a - b
+    return jsonify({"resultado": a - b})
 
-
+@app.route("/multiplicacao/<int:a>/<int:b>")
 def multiplicacao(a, b):
-    return a * b
+    return jsonify({"resultado": a * b})
 
 
 if __name__ == "__main__":
-    while True:
-        print("Rodando...")
-        time.sleep(5)
+    app.run(host="0.0.0.0", port=5000)
