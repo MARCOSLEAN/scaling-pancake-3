452d2a7f5ceec6bde6c997ea2d153019c9ee31a9
Marcos Leandro Cachinski <47596246+MARCOSLEAN@users.noreply.github.com>
2026-03-30 19:51:15 -0300

    ajuste no app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return jsonify({"message": "App rodando com Docker"})
 
+
 @app.route("/soma/<int:a>/<int:b>")
 def soma(a, b):
     return jsonify({"resultado": a + b})
 
+
 @app.route("/subtracao/<int:a>/<int:b>")
 def subtracao(a, b):
     return jsonify({"resultado": a - b})
+
 
 @app.route("/multiplicacao/<int:a>/<int:b>")
 def multiplicacao(a, b):
@@ -21,3 +26,4 @@ def multiplicacao(a, b):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    
